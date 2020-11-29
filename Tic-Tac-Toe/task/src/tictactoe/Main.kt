7b94fb597bb15f993998a1b95b69931025d929b0
Aras <amvitkus@gmail.com>
2020-11-29 14:13:00 -0500

    Completed Stage 2, reading a string of nine symbols and displaying a board.

--- Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,8 +1,17 @@
 package tictactoe
 
+import java.util.*
+
 fun main() {
     // write your code here
-    println("X O X")
-    println("O X O")
-    println("X X O")
+    val scanner = Scanner(System.`in`)
+
+    println("Enter cells: ")
+    val symbols = scanner.next()
+
+    println("---------")
+    println("| " + "${symbols[0]} " + "${symbols[1]} " + "${symbols[2]} " + "|")
+    println("| " + "${symbols[3]} " + "${symbols[4]} " + "${symbols[5]} " + "|")
+    println("| " + "${symbols[6]} " + "${symbols[7]} " + "${symbols[8]} " + "|")
+    println("---------")
 }
