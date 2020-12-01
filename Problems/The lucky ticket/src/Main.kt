99d804cb2df8aec3ced225e15762d16340f2c549
Aras <amvitkus@gmail.com>
2020-12-01 01:44:29 -0500

    Declaring function for division of two Longs, returning a double.

--- Problems/The lucky ticket/src/Main.kt
+++ Problems/The lucky ticket/src/Main.kt
@@ -1,6 +1,21 @@
+import java.util.*
+
 fun main() {
     // write your code here
+    val scanner = Scanner(System.`in`)
 
-    val charValue = '0'
-    val intValue0 = charValue.toString().toInt() // 0
+    val ticketArray  = IntArray(scanner.nextInt())
+    val half1 = ticketArray[0] + ticketArray[1] + ticketArray[2]
+    val half2 = ticketArray[3] + ticketArray[4] + ticketArray[5]
+
+    println(half1)
+    println(half2)
+
+    if (half1 == half2) {
+        println("Lucky")
+    } else {
+        println("Regular")
+    }
+
+    //println(ticketArray.toString())
 }
