3a7d2fb8602d0ca11ea0da5692c2291004b42d46
Aras <amvitkus@gmail.com>
2020-12-01 13:37:42 -0500

    DNA compression algorithm completed.

---
+++ Problems/Compression algorithm/src/Main.kt
@@ -0,0 +1,20 @@
+
+fun main() {
+    // write your code here
+
+    val input = readLine()!!
+    var output = ""
+    var count = 1
+    for (i in input.indices) {
+        if (i + 1 <= input.length - 1) {
+            if (input[i] == input[i + 1]) {
+                count++
+            } else {
+                output += "${input[i]}$count"
+                count = 1
+            }
+        }
+    }
+    output += "${input.last()}$count"
+    println(output)
+}
