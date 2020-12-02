9c72a04a6c9cbcbe23e7f1d3d2fda40428ca19e5
Aras <amvitkus@gmail.com>
2020-12-02 15:00:44 -0500

    Start of generating password task.

---
+++ Problems/Generating passwords/src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    // write your code here
+
+    val scanner = Scanner(System.`in`)
+    var a = scanner.nextLine()
+    var b = scanner.nextLine()
+    var c = scanner.nextLine()
+    var n = scanner.nextLine()
+
+}
