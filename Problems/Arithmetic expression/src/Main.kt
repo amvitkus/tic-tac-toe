364140f1e5de47341a2b69b558e09fec5f9f273f
Aras <amvitkus@gmail.com>
2020-12-03 11:52:30 -0500

    Arithmetic expression problem of the day solved.

---
+++ Problems/Arithmetic expression/src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    // write your code here
+    val num1 = scanner.nextInt()
+    val num2 = scanner.nextInt()
+    val sum = num1 + num2
+
+    println("$num1 plus $num2 equals $sum")
+}
