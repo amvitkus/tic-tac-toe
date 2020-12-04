f0e4ba7e8362a3f998e404f3cc55c033784054c2
Aras <amvitkus@gmail.com>
2020-12-04 14:30:29 -0500

    Solved problem of the day, check if a number is positive.

---
+++ Problems/Check a number is positive/src/Main.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    // write your code here
+    val num = scanner.nextInt()
+
+    if (num > 0) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
