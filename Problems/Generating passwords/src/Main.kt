56bc8e9b6aacc4d9258065198f87b7e4faf7ba4d
Aras <amvitkus@gmail.com>
2020-12-06 11:41:05 -0500

    More testing on generating passwords.

--- Problems/Generating passwords/src/Main.kt
+++ Problems/Generating passwords/src/Main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 fun main() {
     // write your code here
@@ -9,4 +10,13 @@ fun main() {
     var c = scanner.nextLine()
     var n = scanner.nextLine()
 
+    a = charArrayOf('A', 'B', 'C', 'D', 'Z').toString()
+    b = charArrayOf('a', 'b', 'c', 'd', 'z').toString()
+
+
+    var uppercase = List(25) { arrayOf(a)}
+    var lowercase = List(25) { arrayOf(b)}
+    //var digits = List(99) {Random(0, 100)}
+
+    //println("$uppercase$lowercase$digits")
 }
