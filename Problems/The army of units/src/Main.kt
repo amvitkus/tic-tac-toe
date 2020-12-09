da392d529d5e6c405676fae43a5b54b31df0a5d9
Aras <amvitkus@gmail.com>
2020-12-09 12:04:56 -0500

    Army of units task solved using when without an argument.

--- Problems/The army of units/src/Main.kt
+++ Problems/The army of units/src/Main.kt
@@ -4,4 +4,18 @@ fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     // write your code here
+    val n = scanner.nextInt()
+
+    when {
+        n < 1 -> println("no army")
+        n in 1..4 -> println("few")
+        n in 5..9 -> println("several")
+        n in 10..19 -> println("pack")
+        n in 20..49 -> println("lots")
+        n in 50..99 -> println("horde")
+        n in 100..249 -> println("throng")
+        n in 250..499 -> println("swarm")
+        n in 500..999 -> println("zounds")
+        n > 999 -> println("legion")
+    }
 }
