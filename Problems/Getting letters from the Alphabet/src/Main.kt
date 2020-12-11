273a875c44ddbba34ec06cb1c00c9c09c8478f7e
Aras <amvitkus@gmail.com>
2020-12-10 21:27:01 -0500

    Getting letters from the alphabet task solved.

--- Problems/Getting letters from the Alphabet/src/Main.kt
+++ Problems/Getting letters from the Alphabet/src/Main.kt
@@ -2,5 +2,26 @@ import java.util.Scanner
 
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
-    // put your code here
+    val x = input.next().single()
+    var result = " "
+
+    for (i in 'a'..'z') {
+        if (i != x) {
+            result += i
+        } else
+            return println(result)
+    }
+
+    //Code below is a way of solving it without being forced to use return
+//    val word = input.next()
+//    var result = " "
+//
+//    for (i in 'a'..'z') {
+//        if (i in word) {
+//            break
+//        } else {
+//            result += i
+//        }
+//    }
+//    print(result)
 }
